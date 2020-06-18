361b0c38089df6716852bee2f9e2e0e988c23440
alexey-agafonov <aleks.xploid@gmail.com>
2020-06-18 16:59:10 +0300

    Debug

--- rantier-parser/app.py
+++ rantier-parser/app.py
@@ -10,27 +10,26 @@ import os
 def connect_to_db(url: str, db_name: str) -> MongoClient:
     return MongoClient(url)[db_name]
 
-
-def get_realty(login: str, token: str) -> []:
-    data = list(db['city'].find({'is_supported': True}))
-    cities = ''
-    for city in data:
-        cities += f'{city["name"]}|'
-
-    result = requests.get("https://ads-api.ru/main/api",
-                          params={'user': login,
-                                  'token': token,
-                                  'category_id': '2',
-                                  'city': cities,
-                                  'nedvigimost_type': '2',
-                                  'date1': '2020-06-18 15:00:00'
-                                  })
+    # async def get_realty(login: str, token: str) -> []:
+    # data = list(db['city'].find({'is_supported': True}))
+    # cities = ''
+    # for city in data:
+    #     cities += f'{city["name"]}|'
+    #
+    # result = requests.get("https://ads-api.ru/main/api",
+    #                       params={'user': login,
+    #                               'token': token,
+    #                               'category_id': '2',
+    #                               'city': cities,
+    #                               'nedvigimost_type': '2',
+    #                               'date1': '2020-06-18 15:00:00'
+    #                               })
     # 'startid':
     # date1 and date2
-    return result.json()['data']
+    # return result.json()['data']
 
 
-def init_app():
+async def init_app():
     api_login = os.environ.get('API_LOGIN')
     api_token = os.environ.get('API_TOKEN')
     db_user = os.environ.get('DB_USER')
@@ -39,7 +38,22 @@ def init_app():
     db_name = 'rantier'
 
     db = connect_to_db(db_url, db_name)
-    realty = get_realty(api_login, api_token)
+    # realty = get_realty(api_login, api_token)
+
+    data = list(db['city'].find({'is_supported': True}))
+    cities = ''
+    for city in data:
+        cities += f'{city["name"]}|'
+
+    realty = requests.get("https://ads-api.ru/main/api",
+                          params={'user': api_login,
+                                  'token': api_token,
+                                  'category_id': '2',
+                                  'city': cities,
+                                  'nedvigimost_type': '2',
+                                  'date1': '2020-06-18 15:00:00'
+                                  })
+
     bulk_update = db['realty'].initialize_unordered_bulk_op()
 
     for data in realty:
